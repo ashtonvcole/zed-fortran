a56b5bb2d1ee73bc29b13b2c3f1181539e3b566a
Ashton Cole <ashtoncole@Ashtons-MacBook-Pro.local>
2026-06-01 00:47:44 -0500

    Fixed indentation

--- languages/fortran-free/indents.scm
+++ languages/fortran-free/indents.scm
@@ -1,31 +1,27 @@
-[
-  (module)
-  (submodule)
-  (program)
-  (subroutine)
-  (module_procedure)
-  (function)
-  ; (interface)
-  (if_statement)
-  (do_loop_statement)
-  (where_statement)
-  (derived_type_definition)
-  (enum)
-] @indent
+;; Program structuring blocks
+((interface (end_interface_statement) @end) @indent)
+((module (end_module_statement) @end) @indent)
+((submodule (end_submodule_statement) @end) @indent)
+((program (end_program_statement) @end) @indent)
 
+;; Functions, subroutines, and procedures
+((subroutine (end_subroutine_statement) @end) @indent)
+((module_procedure (end_module_procedure_statement) @end) @indent)
+((function (end_function_statement) @end) @indent)
+
+;; Conditionals
+((if_statement (end_if_statement) @end) @indent)
 [
-  (end_module_statement)
-  (end_submodule_statement)
-  (end_program_statement)
-  (end_subroutine_statement)
-  (end_module_procedure_statement)
-  (end_function_statement)
-  ; (end_interface_statement)
-  (end_if_statement)
-  (end_do_loop_statement)
   (else_clause)
   (elseif_clause)
-  (end_type_statement)
-  (end_enum_statement)
-  (end_where_statement)
-] @branch
+] @outdent
+
+;; Loops
+((do_loop_statement (end_do_loop_statement) @end) @indent)
+((where_statement (end_where_statement) @end) @indent)
+
+;; Derived types
+((derived_type_definition (end_type_statement) @end) @indent)
+
+;; Enums
+((enum (end_enum_statement) @end) @indent)
